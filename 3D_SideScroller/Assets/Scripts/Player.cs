f318e890ae3ed32306a401636e135e89840460cc
coderx <36203292+blcx@users.noreply.github.com>
2020-06-19 22:20:32 +0530

    prototype
    walk and run

---
+++ 3D_SideScroller/Assets/Scripts/Player.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+
+    public Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Movement();
+
+        
+    }
+
+
+
+
+
+    void Movement()
+    {
+
+        anim.SetFloat("Horizontal", CrossPlatformInputManager.GetAxis("Horizontal"));
+
+        if (CrossPlatformInputManager.GetAxis("Horizontal") < 0f)
+        {
+            transform.RotateAround(transform.position, transform.up, 180f);
+
+
+        }
+        else if(CrossPlatformInputManager.GetAxis("Horizontal") > 0f)
+        {
+            transform.RotateAround(transform.position, transform.up, 180f);
+
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
