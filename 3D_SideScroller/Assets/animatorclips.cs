883be6c1f591ab16c228db938b665b97d08a204d
coderx <36203292+blcx@users.noreply.github.com>
2020-06-25 03:38:14 +0530

    issue issue issue

--- 3D_SideScroller/Assets/animatorclips.cs
+++ 3D_SideScroller/Assets/animatorclips.cs
@@ -26,16 +26,11 @@ public class animatorclips : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
          player.rb.isKinematic = false;
         player.transform.parent = null;
          player.anim.SetBool("Climb", false);
-        player.transform.position = new Vector3(player.EdgePos.position.x, player.EdgePos.position.y + 0.2f, 0);
-
-        
+        player.transform.position = new Vector3(player.EdgePos.position.x, player.EdgePos.position.y + 0.2f, 0);      
      
-
-
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
